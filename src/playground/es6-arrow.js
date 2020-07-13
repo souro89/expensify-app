64750c5745117d3b2452712c206feccd32a975ef
souro89 <souro89@gmail.com>
2020-07-13 18:57:56 +0400

    boilerplate

--- src/playground/es6-arrow.js
+++
@@ -1,9 +0,0 @@
-const multiplier = {
-  numbers: [1, 2, 3],
-  multiplyBy: 2,
-  multiply() {
-    return this.numbers.map((n) => n * this.multiplyBy);
-  },
-};
-
-console.log(multiplier.multiply());
